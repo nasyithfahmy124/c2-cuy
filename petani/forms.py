65d31ecebc5e2f4077c1cac060e1868ab350260f
Nasyith Fahmy <nasyithfahmy124@gmail.com>
2026-04-19 23:15:27 +0700

    test

--- petani/forms.py
+++ petani/forms.py
@@ -9,4 +9,4 @@ class FormLaporan(forms.ModelForm):
     class Meta:
         model = Laporan
         fields = ['judul', 'deskripsi', 'jumlah_pengeluaran', 'bukti']
-        # exclude = ['estimasi_hasil','dana_terkumpul','status','luas_lahan',]
+        
